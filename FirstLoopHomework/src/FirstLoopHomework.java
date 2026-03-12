11f36d275fd1f8330b9c2953fbe796bac67687bb
nagypalgabor1991-cloud <nagypalgabor1991@gmail.com>
2026-03-12 21:00:47 +0100

    first loop homework

---
+++ FirstLoopHomework/src/FirstLoopHomework.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class FirstLoopHomework {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter a text:");
+        String text = scanner.nextLine();
+        System.out.println("Please enter a number:");
+        int n = scanner.nextInt();
+        scanner.nextLine();
+        if (n < 0) {
+            System.err.println("Invalid input.");
+        } else {
+            for (int i = 0; i < 10; i++) {
+                System.out.println(text);
+            }
+        }
+    }
+}
